f434e01191c3e652ebd66dd3e6e7b836cc6fd03b
unknown <byampols@alumni.cmu.edu>
2021-08-17 15:46:30 -0400

    used localStorage to store a high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -193,7 +193,16 @@ var startGame = function() {
 var endGame = function() {
     //if player is still alive, player wins!
     if(playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+        window.alert("The game has now ended. Let's see how you did!");
+
+        var highScore = localStorage.getItem("High-Score") || 0;
+        if (playerInfo.money >= highScore) {
+            localStorage.setItem("High-Score", playerInfo.money);
+            localStorage.setItem("name", playerInfo.name);
+            window.alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+        } else {
+            window.alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+        }
     }
     else {
         window.alert("You've lost your robot in battle.");
